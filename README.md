315255c617afa97d28410a10b99a5dea3152a4fe
n2ref <n2ref2@gmail.com>
2024-06-22 13:20:35 +0300

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# coreui-form-field-select2-php
+# Coreui form field select2
+
+[Online documentation](https://n2ref.github.io/coreui-form-field-select2)
+
+
+### Install
+
+```shell
+composer install n2ref/coreui-form-field-select2-php
+```
+
+
+### Select2 options
+
+[select2.org](https://select2.org)
+
+
+### Example usage
+
+```php
+$form = new Form();
+
+$form->setRecord([
+    'field' => 'Reactive',
+]);
+
+$options = [
+    'Reactive',
+    'Solution',
+    'Conglomeration',
+    'Algoritm',
+    'Holistic',
+];
+
+$form->setFields([
+    (new Field\Select2('field', 'title')))->setWidth(300)->setOptions($options)
+        ->setSelect2([
+            "placeholder"     => 'Write your value',
+            "tags"            => true,
+            "tokenSeparators" => [',', ' ']
+        ]),
+]);
+
+echo json_encode($form->toArray());
+```
+
+Output
+```json
+{
+    "component": "coreui.form",
+    "record"  : {
+      "field": "Reactive"
+    },
+    "fields"  : [
+      
+      {
+        "type": "select2", 
+        "name": "field", 
+        "label": "title", 
+        "width": 300, 
+        "options": [
+          "Reactive", "Solution", "Conglomeration", "Algoritm", "Holistic"
+        ], 
+        "select2": {
+            "placeholder": "Write your value",
+            "tags": true,
+            "tokenSeparators": [",", " "]
+          }
+      }
+      
+    ]
+}
+```
---
+++ src/Field/Select2.php
@@ -0,0 +1,123 @@
+<?php
+namespace CoreUI\Form\Field;
+use CoreUI\Form\Abstract;
+use CoreUI\Form\Trait;
+
+
+/**
+ *
+ */
+class Select2 extends Abstract\Field {
+
+    use Trait\Name;
+    use Trait\Label;
+    use Trait\Description;
+    use Trait\DescriptionHelp;
+    use Trait\Width;
+    use Trait\WidthLabel;
+    use Trait\Attributes;
+    use Trait\Required;
+    use Trait\FieldReadonly;
+    use Trait\InvalidText;
+    use Trait\ValidText;
+    use Trait\OptionsSelect;
+    use Trait\Show;
+    use Trait\Fields;
+    use Trait\NoSend;
+
+    protected ?array $select2 = null;
+
+
+    /**
+     * @param string      $name
+     * @param string|null $label
+     */
+    public function __construct(string $name, string $label = null) {
+
+        $this->setName($name);
+        $this->setLabel($label);
+    }
+
+
+    /**
+     * Установка настроек для select2
+     * @param array|null $select2
+     * @return self
+     */
+    public function setSelect2(array $select2 = null): self {
+        $this->select2 = $select2;
+        return $this;
+    }
+
+
+    /**
+     * Преобразование в массив
+     * @return array
+     */
+    public function toArray(): array {
+
+        $result = [
+            'type' => 'select'
+        ];
+
+        if ( ! is_null($this->name)) {
+            $result['name'] = $this->name;
+        }
+        if ( ! is_null($this->label)) {
+            $result['label'] = $this->label;
+        }
+        if ( ! is_null($this->description_help)) {
+            $result['descriptionHelp'] = $this->description_help;
+        }
+        if ( ! is_null($this->description)) {
+            $result['description'] = $this->description;
+        }
+        if ( ! is_null($this->required)) {
+            $result['required'] = $this->required;
+        }
+        if ( ! is_null($this->readonly)) {
+            $result['readonly'] = $this->readonly;
+        }
+        if ( ! is_null($this->width)) {
+            $result['width'] = $this->width;
+        }
+        if ( ! is_null($this->width_label)) {
+            $result['widthLabel'] = $this->width_label;
+        }
+        if ( ! is_null($this->invalid_text)) {
+            $result['invalidText'] = $this->invalid_text;
+        }
+        if ( ! is_null($this->valid_text)) {
+            $result['validText'] = $this->valid_text;
+        }
+        if ( ! is_null($this->show)) {
+            $result['show'] = $this->show;
+        }
+        if ( ! is_null($this->position)) {
+            $result['position'] = $this->position;
+        }
+        if ( ! is_null($this->attr)) {
+            $result['attr'] = $this->attr;
+        }
+        if ( ! is_null($this->options_select)) {
+            $result['options'] = $this->options_select;
+        }
+        if ( ! is_null($this->no_send)) {
+            $result['noSend'] = $this->no_send;
+        }
+
+        if ( ! is_null($this->fields)) {
+            $fields = [];
+
+            foreach ($this->fields as $field) {
+                if ($field instanceof Abstract\Field) {
+                    $fields[] = $field->toArray();
+                }
+            }
+
+            $result['fields'] = $fields;
+        }
+
+        return $result;
+    }
+}
